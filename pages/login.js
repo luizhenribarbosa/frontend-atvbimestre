0ab0673cc05849e522998d95bd47bb853c3c374f
soso <luiz.oliveirabarbosa@eaportal.org>
2025-12-01 15:24:00 -0300

    frontend

---
+++ pages/login.js
@@ -0,0 +1,39 @@
+const button = document.querySelector("button")
+
+button.onclick = (event) => {
+    event.preventDefault()
+    login()
+}
+
+async function login() {
+    const email = document.querySelector("#email").value
+    const password = document.querySelector("#senha").value
+
+    const user = {
+        email,
+        password
+    }
+
+    const response = await fetch("http://localhost:3333/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({user})
+    }).then(response => response.json())
+
+
+    if(response.message) {
+        alert(response.message)
+        return
+    }
+
+    const {id, name} = response
+
+    sessionStorage.setItem("user", JSON.stringify({id, name}))
+
+    alert("Login realizado com sucesso!")
+
+
+    window.location.href = "../index.html"
+}
---
+++ script.js
@@ -0,0 +1,31 @@
+function init() {
+    const navUl = document.querySelector("nav ul")
+
+    const user = JSON.parse(sessionStorage.getItem("user"))
+
+    //verifica se existe um usuario logado
+    if (user) {
+        navUl.innerHTML += `
+            <li>
+                <a href="./pages/jogar.html">Jogar</a>
+            </li>
+        
+            <li>
+                <h2>Usuário: ${user.name}</h2>
+            </li>
+            <li>
+                <button>Sair</button>
+            </li>
+            
+        `
+        return
+    }
+
+    navUl.innerHTML += `
+        <li>
+            <a href="./pages/login.html">Login</a>
+        </li>
+    `
+}
+
+init()
